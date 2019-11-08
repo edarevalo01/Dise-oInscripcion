07947c4c89a43eef2f077fc3966eaf464637bb6a
Alejandro <edarevalo01@gmail.com>
2019-11-08 10:47:36 -0500

    Mejoras diseño

--- src/app/components/pregrado/pregrado.component.ts
+++ src/app/components/pregrado/pregrado.component.ts
@@ -9,33 +9,35 @@ export class PregradoComponent implements OnInit {
   breakpoint: number;
 
   inscripcion: boolean = true;
-  labelBotonIzquierdo: string = 'CONTINUAR';
-  tituloIzquierdo: string = 'Bienevenido de nuevo!';
-  contentIzquierdo: string = 'Estamos listos para llevar tu proceso de admisión al siguiente nivel, en cualquier momento que lo necesites! para mantenerte conectado con nosotros, inicie sesión con su información personal'
-  labelBotonDerecho: string = 'INCRÍBIRME';
-  tituloDerecho: string = 'Inscríbete';
-  contentDerecho: string = 'Ingresa tus datos personales e inicia un viaje estrella paso a paso al programa académico de tú interés';
+  labelBotonDerecho: string = "CONTINUAR";
+  tituloDerecho: string = "Bienevenido de nuevo!";
+  contentDerecho: string =
+    "Estamos listos para llevar tu proceso de admisión al siguiente nivel, en cualquier momento que lo necesites! para mantenerte conectado con nosotros, inicie sesión con su información personal";
+  labelBotonIzquierdo: string = "INCRÍBIRME";
+  tituloIzquierdo: string = "Inscríbete";
+  contentIzquierdo: string = "Ingresa tus datos personales e inicia un viaje estrella paso a paso al programa académico de tú interés";
 
-  
   constructor() {}
-  
-  cambiarRegistro(){
+
+  cambiarRegistro() {
     this.inscripcion = !this.inscripcion;
-    if(this.inscripcion){
-      this.labelBotonIzquierdo = 'CONTINUAR';
-      this.tituloIzquierdo = 'Bienevenido de nuevo!';
-      this.contentIzquierdo = 'Estamos listos para llevar tu proceso de admisión al siguiente nivel, en cualquier momento que lo necesites! para mantenerte conectado con nosotros, inicie sesión con su información personal'
-      this.labelBotonDerecho = 'INCRÍBIRME';
-      this.tituloDerecho = 'Inscríbete';
-      this.contentDerecho = 'Ingresa tus datos personales e inicia un viaje estrella paso a paso al programa académico de tú interés';
-    }
-    else{
-      this.labelBotonIzquierdo = 'INCRÍBIRME';
-      this.tituloIzquierdo = 'Proceso de admisión!';
-      this.contentIzquierdo = 'Donec imperdiet lacus eget tellus tincidunt, ut fringilla elit blandit. Aliquam mollis erat quis lacinia hendrerit. Cras consectetur purus nec est tempor, quis faucibus diam vestibulum.'
-      this.labelBotonDerecho = 'CONTINUAR';
-      this.tituloDerecho = 'Bienevenido de nuevo!';
-      this.contentDerecho = 'Estamos listos para llevar tu proceso de admisión al siguiente nivel, en cualquier momento que lo necesites!';
+    if (this.inscripcion) {
+      this.labelBotonDerecho = "CONTINUAR";
+      this.tituloDerecho = "Bienevenido de nuevo!";
+      this.contentDerecho =
+        "Estamos listos para llevar tu proceso de admisión al siguiente nivel, en cualquier momento que lo necesites! para mantenerte conectado con nosotros, inicie sesión con su información personal";
+      this.labelBotonIzquierdo = "INCRÍBIRME";
+      this.tituloIzquierdo = "Inscríbete";
+      this.contentIzquierdo = "Ingresa tus datos personales e inicia un viaje estrella paso a paso al programa académico de tú interés";
+    } else {
+      this.labelBotonDerecho = "INCRÍBIRME";
+      this.tituloDerecho = "Proceso de admisión!";
+      this.contentDerecho =
+        "Donec imperdiet lacus eget tellus tincidunt, ut fringilla elit blandit. Aliquam mollis erat quis lacinia hendrerit. Cras consectetur purus nec est tempor, quis faucibus diam vestibulum.";
+      this.labelBotonIzquierdo = "CONTINUAR";
+      this.tituloIzquierdo = "Bienevenido de nuevo!";
+      this.contentIzquierdo =
+        "Estamos listos para llevar tu proceso de admisión al siguiente nivel, en cualquier momento que lo necesites!";
     }
   }
 
