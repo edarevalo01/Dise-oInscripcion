69f9b2476b8e753a856be4ba42f5f3d782802205
Alejandro <edarevalo01@gmail.com>
2019-11-18 11:07:41 -0500

    PseudoCambios - Grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,41 @@ import { UrlTree, Router } from "@angular/router";
 export class AppComponent {
   title = "InscripcionLasalle";
   formReducido: boolean = false;
+  formPeq: boolean = false;
+  pantalla: number = 8;
+  altura: number = 0;
+  gridClass: string = "";
 
   constructor(private router: Router) {
     var paramUrl = this.obtenerParametro("programa");
     if (paramUrl != 0) {
       this.formReducido = true;
+      this.formPeq = true;
+      this.pantalla = 6;
+      this.altura = 860;
+      this.gridClass = "grid-styles-reducido";
+    } else {
+      this.pantalla = window.innerWidth <= 540 ? 6 : 8;
+      this.resize();
+    }
+  }
+
+  public onResize(event) {
+    if (!this.formReducido) {
+      this.pantalla = event.target.innerWidth <= 540 ? 6 : 8;
+      this.resize();
+    }
+  }
+
+  resize() {
+    if (this.pantalla == 6) {
+      this.formPeq = true;
+      this.gridClass = "grid-styles-reducido";
+      this.altura = 860;
+    } else {
+      this.formPeq = false;
+      this.gridClass = "grid-styles-complete";
+      this.altura = 663;
     }
   }
 
